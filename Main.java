82fc96535e6e6c0509ebd8c004a88b2c92a77312
g-madhu4 <madhu.amma9133@gmail.com>
2024-10-29 08:19:15 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,68 +1,184 @@
 import java.lang.*;
 import java.util.*;
 class main{
-    public static void main(String[] args) {
-        int[][]sudoku=new int[9][9];
-        making(sudoku,0,0);
+    public static void main(String[] args) throws Exception{
+        String aa="aab";
+        huffman hf=new huffman(aa);
+        String encode= hf.encode(aa);
+        System.out.println(encode);
+        String decode= hf.decode(encode);
+        System.out.println(decode);
+
+    }
+}
+class huffman{
+HashMap<Character,String>encoder;
+HashMap<String,Character>decoder;
+private class node implements Comparable<node>{
+    Character data;
+    int cost;
+    node left;
+    node right;
+    public node(Character data,int cost){
+        this.data=data;
+        this.cost=cost;
+        this.left=null;
+        this.right=null;
+    }
+    public int compareTo(node other){
+        return this.cost-other.cost;
+    }
+}
+public huffman(String feeder) throws Exception{
+    HashMap<Character,Integer>fmap=new HashMap<>();
+    for(int i=0;i<feeder.length();i++){
+        char cc=feeder.charAt(i);
+        fmap.putIfAbsent(cc,0);
+        fmap.put(cc,fmap.get(cc)+1);
+    }
+    Heap<node>minheap=new Heap<>();
+
+    Set<Map.Entry<Character,Integer>>entrySet= fmap.entrySet();
+    for(Map.Entry<Character,Integer>entry:entrySet ) {
+        node node = new node(entry.getKey(), entry.getValue());
+        minheap.insert(node);
+    }
+    while(minheap.size()!=1){
+        node first=minheap.remove();
+        node second=minheap.remove();
+        node newnode=new node('\0',first.cost+second.cost);
+        newnode.left=first;
+        newnode.right=second;
+        minheap.insert(newnode);
+
+    }
+    node ft=minheap.remove();
+    this.encoder=new HashMap<>();
+    this.decoder=new HashMap<>();
+    this.iniEncoder(ft,"");
+}
+public void iniEncoder(node node,String osf){
+    if(node==null)
+    {
+        return;
+    }
+    if(node.left==null&&node.right==null){
+        this.encoder.put(node.data,osf);
+        this.decoder.put(osf,node.data);
     }
 
-    private static void making(int[][] sudoku, int row, int col) {
-        if(row==sudoku.length){
-            print(sudoku);
-            System.out.println("mmmmmm");
+    iniEncoder(node.left,osf+"0");
+    iniEncoder(node.right,osf+"1");
+}
+public String encode(String data) {
+    String ans = "";
+    for (int i = 0; i < data.length(); i++) {
+        ans += encoder.get(data.charAt(i));
+    }
+    return ans;
+}
+public String decode(String data){
+    String ans="";
+    String res="";
+    for(int i=0;i<data.length();i++){
+        ans+=data.charAt(i);
+        if(decoder.containsKey(ans)){
+            res+=decoder.get(ans);
+            ans="";
+        }
+    }
+    return res;
+}
+
+
+
+ }
+
+class Heap<T extends Comparable<T>> {
+
+    private ArrayList<T> list;
+
+    public Heap() {
+        list = new ArrayList<>();
+    }
+
+    private void swap(int first, int second) {
+        T temp = list.get(first);
+        list.set(first, list.get(second));
+        list.set(second, temp);
+    }
+
+    private int parent(int index) {
+        return (index - 1) / 2;
+    }
+
+    private int left(int index) {
+        return index * 2 + 1;
+    }
+
+    private int right(int index) {
+        return index * 2 + 2;
+    }
+
+    public void insert(T value) {
+        list.add(value);
+        upheap(list.size() - 1);
+    }
+
+    private void upheap(int index) {
+        if (index == 0) {
             return;
         }
-        if(col==sudoku.length){
-            making(sudoku,row+1,0);
-            return;
-        }
-        if(sudoku[row][col]!=0){
-            making(sudoku,row,col+1);
-        }
-        else {
-            for (int i = 1; i < 10; i++) {
-                if (isOkay(sudoku, row, col, i)) {
-                    sudoku[row][col] = i;
-                    making(sudoku, row, col + 1);
-                    sudoku[row][col] = 0;
-
-                }
-            }
-        }
-
-    }
-
-    private static void print(int[][] sudoku) {
-        for(int[]a:sudoku){
-            for(int x:a){
-                System.out.print(x+" ");
-            }
-            System.out.println(" ");
+        int p = parent(index);
+        if (list.get(index).compareTo(list.get(p)) < 0) {
+            swap(index, p);
+            upheap(p);
         }
     }
 
+    public T remove() throws Exception {
+        if (list.isEmpty()) {
+            throw new Exception("Removing from an empty heap!");
+        }
 
-    private static boolean isOkay(int[][] sudoku, int row, int col, int val) {
-        for(int i=0;i<sudoku.length;i++){
-            if(sudoku[row][i]==val){
-                return false;
-            }
+        T temp = list.get(0);
+
+        T last = list.remove(list.size() - 1);
+        if (!list.isEmpty()) {
+            list.set(0, last);
+            downheap(0);
         }
-        for(int i=0;i<sudoku.length;i++){
-            if(sudoku[i][col]==val){
-                return false;
-            }
-        }
-        int cg=col/3;
-        int ro=row/3;
-        for(int i=ro*3;i<=ro*3+2;i++){
-            for(int j=cg*3;j<=cg*3+2;j++){
-                if(sudoku[i][j]==val){
-                    return false;
-                }
-            }
-        }
-        return true;
+
+        return temp;
     }
 
+    private void downheap(int index) {
+        int min = index;
+        int left = left(index);
+        int right = right(index);
+
+        if (left < list.size() && list.get(min).compareTo(list.get(left)) > 0) {
+            min = left;
+        }
+
+        if (right < list.size() && list.get(min).compareTo(list.get(right)) > 0) {
+            min = right;
+        }
+
+        if (min != index) {
+            swap(min, index);
+            downheap(min);
+        }
+    }
+    public ArrayList<T> heapSort() throws Exception {
+        ArrayList<T> data = new ArrayList<>();
+        while(!list.isEmpty()) {
+            data.add(this.remove());
+        }
+        return data;
+    }
+
+    public int size() {
+       return list.size();
+    }
 }
